c02337277a79a036698d9c186a4ebc64f78f980f
Adekunle <adefabiyusuf@gmail.com>
2023-11-06 21:11:49 -0600

    Actuator Addition for expose health and info metrics and add application.dev.yml

--- backend/src/main/java/com/adekunle/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/adekunle/security/SecurityFilterChainConfig.java
@@ -36,7 +36,7 @@ public class SecurityFilterChainConfig {
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST,"/api/v1/customers", "/api/v1/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/ping").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/ping","/actuator/**", "/error").permitAll()
                         .anyRequest().authenticated()
                 ).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
